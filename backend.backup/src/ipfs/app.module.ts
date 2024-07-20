5e3311f7f335cb6efd3e21be90fbdb23facd102c
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-07-20 02:49:24 +0200

    [REVERT] That is why we shouldn't use git add -A too fast

--- backend.backup/src/ipfs/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IpfsController } from './ipfs.controller';
-import { IpfsService } from './ipfs.service';
-
-@Module({
-  controllers: [IpfsController],
-  providers: [IpfsService],
-})
-export class IpfModule {}
