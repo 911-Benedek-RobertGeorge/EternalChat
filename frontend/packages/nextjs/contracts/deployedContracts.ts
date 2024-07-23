c2ce6f60c2cffb2d31eca54fe1d13c76e59cce73
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-07-23 16:44:04 +0200

    [UPD] Avatars on the chat

--- frontend/packages/nextjs/contracts/deployedContracts.ts
+++ frontend/packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     Chronos: {
-      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
       abi: [
         {
           inputs: [
@@ -652,7 +652,7 @@ const deployedContracts = {
       },
     },
     EthernalChat: {
-      address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      address: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
       abi: [
         {
           inputs: [
@@ -1484,7 +1484,7 @@ const deployedContracts = {
       },
     },
     EthernalChat: {
-      address: "0xBA882C5F8EB1e02dF7AAAaBBC6eD0A449f59C2E9",
+      address: "0xA11f190Fe148f34F023cd99788c306Bf71Af8e27",
       abi: [
         {
           inputs: [
