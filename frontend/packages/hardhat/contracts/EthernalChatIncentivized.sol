e6e7fd2f548ef100584d318d543715ac96ad87cf
Ayodeji Komolafe <kvngeko@icloud.com>
2024-07-23 16:03:33 -0400

    [WIP] resolved nico's review

--- frontend/packages/hardhat/contracts/EthernalChatIncentivized.sol
+++ frontend/packages/hardhat/contracts/EthernalChatIncentivized.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { Chronos } from "./Chronos.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @title A simple chat contract that stores a link between users and messages on IPFS
@@ -17,14 +16,15 @@ contract EthernalChatIncentivized is Ownable {
 		uint64 sizeOfChunks; // Help checking the size of the data during proof verification
 		//  Address of the storage provider we want to incentivize
 		address storageProvider; // This could be a list
-		uint256 allocatedTokens;
+		uint256 timeRewardRedeemed;
+		uint256 totalEthEarned; // total eth earned per stored messages for each account
+		uint256 allocatedEth;
+		uint256 lastWithdrawTime;
 	}
-	uint256 ratio = 3;
+	/// @notice Amount of eth earned for storing a message
+	uint256 public constant ETH_PER_CID = 0.01 ether;
 	/// @notice Mapping of CID/DataInfo of the stored messages for each account
 	mapping(address => DataInfo) private mapDataInfo;
-
-	/// @notice Address of the token used as payment for storing the messages
-	IERC20 public paymentToken;
 
 	/// @notice Amount of tokens required for storing a message
 	event CIDUpdated(address indexed user, bytes32 cid);
@@ -38,10 +38,7 @@ contract EthernalChatIncentivized is Ownable {
 	}
 
 	/// @notice Constructor function
-	/// @param tokenContractAddress  Address of the token used for payment
-	constructor(Chronos tokenContractAddress) Ownable(msg.sender) {
-		paymentToken = IERC20(tokenContractAddress);
-	}
+	constructor() Ownable(msg.sender) {}
 
 	/// @notice Sets the CID (Content Identifier) for the sender of the transaction.
 	/// @param cid The CID to set for the user.
@@ -60,30 +57,38 @@ contract EthernalChatIncentivized is Ownable {
 		bytes32 merkleRootOfAppendedData
 	) public {
 		require(cid != bytes32(0), "Invalid CID");
+		// require(timeRewardRedeemed == block.timestamp);
 		DataInfo memory dataInfo = mapDataInfo[msg.sender];
 		require(
-			dataInfo.cid == bytes32(0) ||
-				dataInfo.merkleRoot == merkleRootOfAppendedData,
+			(dataInfo.cid == bytes32(0) &&
+				dataInfo.merkleRoot == merkleRootOfAppendedData) ||
+				(dataInfo.cid != bytes32(0) &&
+					newMerkleRoot ==
+					keccak256(
+						abi.encodePacked(
+							dataInfo.merkleRoot,
+							merkleRootOfAppendedData
+						)
+					)),
 			"Data not appended correctly"
 		);
-		require(newMerkleRoot != bytes32(0), "invalid merkle root");
+		require(newMerkleRoot != bytes32(0), "Invalid Merkle Root");
 
 		dataInfo.cid = cid;
 		dataInfo.numberOfChunks = numberOfChunks;
 		dataInfo.sizeOfChunks = sizeOfChunks;
 		dataInfo.merkleRoot = newMerkleRoot;
-
+		dataInfo.timeRewardRedeemed = block.timestamp;
+		dataInfo.totalEthEarned += ETH_PER_CID;
 		emit CIDUpdated(msg.sender, cid);
 	}
 
 	/// @notice Add funds to an existing DataInfo by sending tokens and updating
-	/// @param newPrice The new price in tokens
 	/// @dev Make sure the DataInfo is non zero (has been already created)
-	function addFundsForStorage(uint256 newPrice) public {
+	function addFundsForStorage() public payable {
 		DataInfo memory dataInfo = mapDataInfo[msg.sender];
 		require(dataInfo.cid != bytes32(0), "DataInfo not found");
-		paymentToken.transferFrom(msg.sender, address(this), newPrice);
-		dataInfo.allocatedTokens += newPrice;
+		dataInfo.allocatedEth += msg.value;
 	}
 
 	/// @notice Retrieves the CID (Content Identifier) stored for the user.
@@ -102,39 +107,35 @@ contract EthernalChatIncentivized is Ownable {
 			mapDataInfo[addr].cid != bytes32(0),
 			"No DataInfo found for this address"
 		);
-		index = uint64(
-			uint256(
-				keccak256(abi.encodePacked(block.timestamp, block.prevrandao))
-			) % mapDataInfo[addr].numberOfChunks
-		);
+		index = uint64(block.prevrandao) % mapDataInfo[addr].numberOfChunks;
 	}
 
 	/// @notice Allows storage to get a partial amount of reward in token, if it hasn't been already taken in this amount of time
-	function getStorageReward() public view returns (uint256) {
-		uint256 timePassed = 24 hours;
+	function getStorageReward(address addr) public {
+		DataInfo memory dataInfo = mapDataInfo[addr];
 		require(
-			block.timestamp == block.timestamp + timePassed,
-			"Enough time hasn't passed"
+			block.timestamp >= dataInfo.timeRewardRedeemed + 1 days,
+			"Not Enough time has passed"
 		);
 		require(
-			mapDataInfo[msg.sender].cid != bytes32(0),
-			"No Datainfo stored by this address"
+			mapDataInfo[addr].cid != bytes32(0),
+			"No DataInfo store by this address"
 		);
-		DataInfo memory dataInfo = mapDataInfo[msg.sender];
-		return dataInfo.allocatedTokens;
+		//require the proofOfChallenge
+		mapDataInfo[addr].totalEthEarned += ETH_PER_CID / 30;
 	}
 
 	/// @notice Take out all the rewards in Eth based on the amount of tokens the address holds
 	function withdrawRewards() public {
 		DataInfo memory dataInfo = mapDataInfo[msg.sender];
-		uint256 amount = dataInfo.allocatedTokens;
+		uint256 amount = dataInfo.totalEthEarned;
 		require(amount > 0, "No rewards available");
-
-		dataInfo.allocatedTokens = 0;
-		paymentToken.burnFrom(msg.sender, amount);
-		(bool success, ) = payable(msg.sender).call{ value: amount / ratio }(
-			""
+		require(
+			block.timestamp >= dataInfo.lastWithdrawTime + 30 days,
+			"Not Enough time has passed"
 		);
+		dataInfo.totalEthEarned = 0;
+		(bool success, ) = payable(msg.sender).call{ value: amount }("");
 		require(success);
 	}
 }
